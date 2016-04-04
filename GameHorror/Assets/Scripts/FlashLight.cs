3f3a201e7be941b28adb8fb0b02a9e65baa18a56
kevin <kn222gn@student.lnu.se>
2016-04-04 15:54:09 +0200

    Added: function for spotlight

--- GameHorror/Assets/Scripts/FlashLight.cs
+++ GameHorror/Assets/Scripts/FlashLight.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class FlashLight : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public Light spotlight;
+       
+    // Use this for initialization
+    void Start () {
+        spotlight.enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        bool F = Input.GetKeyDown(KeyCode.F);
+
+        if (F && spotlight.enabled == false)
+        {
+            spotlight.enabled = true;
+        }
+        else if (F && spotlight.enabled == true)
+        {
+            spotlight.enabled = false;
+        }
+    }
 }
