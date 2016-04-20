1bc29a794a060ff1ac53293b612b740ab7792aae
kevin <kn222gn@student.lnu.se>
2016-04-20 16:36:09 +0200

    Added some code for bobbing and sound for steps needs tuning

--- GameHorror/Assets/Scripts/Character.cs
+++ GameHorror/Assets/Scripts/Character.cs
@@ -8,18 +8,26 @@ public class Character : MonoBehaviour {
     public float mouseSensitivity = 5.00f;
     float verticalRotation = 0f; //will help with the vertical rotation of the camera  
     public float upDownRate = 60.0f;//sets the rate that the camera will max go to best is 80.  
-    public float jumpSpeed = 5.0f;
+    public float jumpSpeed = 1.0f;
     public float gravity = 20.0f;
+    public float runSpeed = 3.0f;
+    public AudioSource footstep;
+    public AudioSource footstepSprint;
     private Vector3 moveDirection = Vector3.zero;
 
     CharacterController cC;
 
     float verticalVelocity = 0;
 
+    private float elapsedTime = 0;
+    public float footstepsPerSecond;
+    public float footstepsPerSecondSprint;
+
     // Use this for initialization  
     void Start()
     {
         //Cursor.lockState = CursorLockMode.Locked;//This hiddes the mouse pointer
+        //footstep = GetComponent<AudioSource>();
         cC = GetComponent<CharacterController>(); //create a new CharacterController
     }
 
@@ -47,15 +55,31 @@ public class Character : MonoBehaviour {
         float forwardMoveSpeed = Input.GetAxis("Vertical") * movementSpeed;
         float SideMoveSpeed = Input.GetAxis("Horizontal") * movementSpeed;
 
-        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+        elapsedTime += Time.deltaTime;
 
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+        if(forwardMoveSpeed != 0 && cC.isGrounded || SideMoveSpeed != 0 && cC.isGrounded)
+        {
+            if(elapsedTime >= 1/footstepsPerSecond)
+            {
+                footstep.Play();
+                elapsedTime = 0;
+            }
+            
+        }
         if(cC.isGrounded && Input.GetButtonDown("Jump") ){
 
             verticalVelocity = jumpSpeed;
         }
         if (Input.GetKey(KeyCode.LeftShift)) //this checks to see if the player is holding left shif
         {
-            forwardMoveSpeed = Input.GetAxis("Vertical") * movementSpeed + 2.0f; //this increases our speed by the multiplier
+            footstep.Stop();
+            if (elapsedTime >= 1 / footstepsPerSecondSprint)
+            {
+                footstepSprint.Play();
+                elapsedTime = 0;
+            }
+            forwardMoveSpeed = Input.GetAxis("Vertical") * movementSpeed + runSpeed; //this increases our speed by the multiplier
         }
 
         Vector3 speed = new Vector3(SideMoveSpeed, verticalVelocity, forwardMoveSpeed); //puts both speeds in the speed variable
